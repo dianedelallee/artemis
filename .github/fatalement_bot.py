120fe26e576e8b6198897f8c9a28b4f1f22102ff
Diane Delallée <diane.delallee@gmail.com>
2021-03-05 17:04:56 +0100

    Update fatalement_bot.py

--- .github/fatalement_bot.py
+++ .github/fatalement_bot.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
 import os
 
-import feedparser
-import pytz
+import requests
 import tweepy
 
 #Twitter keys
@@ -13,22 +12,24 @@ ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 
 
-def _tweet_article(api, version):
-    text = f"A new version of Artemis-css is available! \nYou can read see the version {version} on https://github.com/dianedelallee/artemis"
+def _tweet_article(api, version, link):
+    text = f"A new version of Artemis-css is available! \nYou can read see the version {version} on {link}"
     print(f'will tweet about the new version of Artemis')
     api.update_status(text)
 
 
 def get_new_version():
     previous_hour = datetime.today().replace(tzinfo=pytz.utc) - timedelta(hours=1)
+    response = requests.get("https://api.github.com/repos/dianedelallee/artemis/releases/latest")
+    publication_date = datetime.strptime(response.json()['published_at'], '%Y-%m-%dT%H:%M:%SZ')
     
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    # find a way to know that a new version is available
-    # called _tweet_article(api, version)
+    if publication_date >= previous_hour:
+    _tweet_article(api, response.json()["tag_name"], response.json()["html_url"])
     print('----- END -----')
 
 
